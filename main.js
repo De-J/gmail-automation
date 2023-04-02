605ef2c181ce9a497635663b839f6a4649567702
De-J <devjtitan@gmail.com>
2023-04-02 20:15:51 +0530

    fix: use await to first finish `getHeader()` execution

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const { showNewMessages, showMessage } = require('./utilities.js');
+const { getHeaders, composeAndSend, showNewMessages, showMessage } = require('./utilities.js');
 const { gmail } = require('./auth.js');
 
 let id = '186f921d25efe376';
+
+//Immediately invoked function expression (or IIFE)
 (async function () {
-  await showNewMessages();
-  showMessage(id);
+  const headers = await getHeaders(id);
+  console.log(headers);
+  composeAndSend(headers);
 })();
+
 
 
 //setInterval(checkForNewEmails, Math.floor(Math.random() * (120 - 45 + 1)) + 45);
