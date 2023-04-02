f633381314590240f512e9032b622baa1034e9ba
De-J <devjtitan@gmail.com>
2023-04-02 16:52:47 +0530

    fix: execute 'showMessage' after completion of 'showNewMessages'

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { getMessage, getNewMessages } = require('./utilities.js');
+const { showNewMessages, showMessage } = require('./utilities.js');
+const { gmail } = require('./auth.js');
 
 let id = '186f921d25efe376';
-
-getNewMessages();
-getMessage(id);
+(async function () {
+  await showNewMessages();
+  showMessage(id);
+})();
 
 
 //setInterval(checkForNewEmails, Math.floor(Math.random() * (120 - 45 + 1)) + 45);
-
